6a1b6924c67bffed6570983ab1f6a6e4b859f43c
xanbalandrau <xanbalandrau@gmail.com>
2025-10-27 14:50:53 +0100

    commit pair impair print

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,13 @@ public class DemoApplication {
 
         System.out.println("Test");
         System.out.println("Commit avant checkout, puis branch dev");
+
+        int chiffre = 1;
+        if(chiffre %2 ==0){
+            System.out.println("Pair");
+        }else {
+            System.out.println("Impair");
+        }
         //SpringApplication.run(DemoApplication.class, args);
 	}
 
